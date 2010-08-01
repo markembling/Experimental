91aa30ce1c6909e9c3d16d1986e276da8587b608
Mark Embling <contact@markembling.info>
2010-08-01 11:58:02 +0100

    Both MongoDbNotes projects now upgraded to Autofac 2.2.

--- MongoDbNotes/src/MongoDbNotes/ContainerRegistrations.cs
+++ MongoDbNotes/src/MongoDbNotes/ContainerRegistrations.cs
@@ -1,15 +1,12 @@
-using Autofac.Builder;
-using Autofac.Integration.Web.Mvc;
+using Autofac;
 using MongoDB.Driver;
 using MongoDbNotes.Models;
 
 namespace MongoDbNotes {
     public class ContainerRegistrations : Module {
         protected override void Load(ContainerBuilder builder) {
-            builder.RegisterModule(new AutofacControllerModule(System.Reflection.Assembly.GetExecutingAssembly()));
-
-            builder.Register(c => MvcApplication.ConnectMongoDb()).ContainerScoped();
-            builder.Register(c => new NoteRepository(c.Resolve<Database>())).ContainerScoped();
+            builder.Register(c => MvcApplication.ConnectMongoDb()).InstancePerLifetimeScope();
+            builder.Register(c => new NoteRepository(c.Resolve<Database>()));
         }
     }
 }
--- MongoDbNotes/src/MongoDbNotes/Global.asax.cs
+++ MongoDbNotes/src/MongoDbNotes/Global.asax.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Services.Description;
-using Autofac.Builder;
+using Autofac;
 using Autofac.Integration.Web;
 using Autofac.Integration.Web.Mvc;
 using MongoDB.Driver;
@@ -54,16 +54,14 @@ namespace MongoDbNotes {
 
         protected void Application_Start() {
             var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
             builder.RegisterModule(new ContainerRegistrations());
             _containerProvider = new ContainerProvider(builder.Build());
+
             ControllerBuilder.Current.SetControllerFactory(new AutofacControllerFactory(ContainerProvider));
 
             AreaRegistration.RegisterAllAreas();
             RegisterRoutes(RouteTable.Routes);
-        }
-
-        protected void Application_EndRequest(object sender, EventArgs e) {
-            ContainerProvider.DisposeRequestContainer();
         }
     }
 }
