a84304abf1b00226c85d8d6cd8b7e9384ff1f88c
Mark Embling <contact@markembling.info>
2010-12-11 22:05:23 +0000

    Circles fade away now. Colour pulled out into its own class.

--- Html5Canvas/Circles/behaviour2.js
+++ Html5Canvas/Circles/behaviour2.js
@@ -5,18 +5,17 @@ $(function() {
 	
 	var x = 100;	
 	
-	function randomColor() {
+	function randomColour() {
 		var r = randomFromTo(20, 80);
 		var g = randomFromTo(30, 120);
 		var b = randomFromTo(40, 120);
-		return "rgba(" + r + "," + g + "," + b + ", 0.5)";
+		return new Colour(r, g, b, 0.5);
 	}
 	
 	function generateCircle() {
 		var circle = new Circle();
 			
-		circle.color = randomColor();
-		circle.movementStep = randomFromTo(2, 8);
+		circle.colour = randomColour();
 		
 		circle.xMovementStep = randomFromTo(-6, 6);
 		circle.yMovementStep = randomFromTo(-6, 6);
@@ -28,8 +27,8 @@ $(function() {
 		return circle;
 	}
 	
-	function generateCircles() {
-		for (var i = 0; i < 30; i++) {
+	function generateCircles(count) {
+		for (var i = 0; i < count; i++) {
 			circles.push(generateCircle());
 		}
 	}
@@ -48,6 +47,14 @@ $(function() {
 				circle.move();
 				circle.draw(context);
 				
+				// Start the fading of nearly-old ones getting near the edge
+				if (circle.x >= (canvas.width() - circle.radius - 60) || circle.x <= (0 + circle.radius + 60) || 
+					circle.y >= (canvas.height() - circle.radius - 60) || circle.y <= (0 + circle.radius + 60)) {
+					
+					circle.fadeOut();
+				}
+				
+				// Kill old ones right at the edge
 				if (circle.x >= (canvas.width() - circle.radius) || circle.x <= (0 + circle.radius) || 
 					circle.y >= (canvas.height() - circle.radius) || circle.y <= (0 + circle.radius)) {
 					
@@ -56,10 +63,12 @@ $(function() {
 				}
 			}
 		}
-		circles.removeMatching(null);
+		
+		// Remove all the killed ones from the circles array
+		removeFromArrayMatching(circles, null);
 	}
 	
-	generateCircles();
+	generateCircles(30);
 	setInterval(function(){
 		animateCircles();
 	}, 50);
@@ -73,7 +82,8 @@ function Circle() {
 	this.radius;
 	this.xMovementStep;
 	this.yMovementStep;
-	this.color;
+	this.colour;
+	this._isFading = false;
 	
 	this.move = function() {
 		this.x += this.xMovementStep;
@@ -84,13 +94,40 @@ function Circle() {
 		// increase radius
 		//this.radius *= 1.04;
 		this.radius += 0.5;
+		
+		// Fading...
+		if (this._isFading)
+			this._performFade();
 	
-		context.fillStyle = this.color;
+		context.fillStyle = this.colour.toString();
 		context.beginPath();
 		// x, y, radius, start angle, end angle, anticlockwise?
 		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
 		context.closePath();
 		context.fill();
+	}
+	
+	this.fadeOut = function() {
+		this._isFading = true;
+	}
+	
+	this._performFade = function() {
+		if (this.colour.a > 0) {
+			this.colour.a = Math.round((this.colour.a - 0.04) * 100) / 100;
+		} else {
+			this.colour.a = 0;
+		}
+	}
+}
+
+function Colour(r, g, b, a) {
+	this.r = r;
+	this.g = g;
+	this.b = b;
+	this.a = a;
+	
+	this.toString = function() {
+		return "rgba(" + this.r + "," + this.g + "," + this.b + ", " + this.a + ")";
 	}
 }
 
@@ -99,7 +136,7 @@ function randomFromTo(from, to){
 }
 
 // http://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
-Array.prototype.removeMatching = function(deleteValue) {
+/*Array.prototype.removeMatching = function(deleteValue) {
   for (var i = 0; i < this.length; i++) {
     if (this[i] == deleteValue) {         
       this.splice(i, 1);
@@ -107,4 +144,12 @@ Array.prototype.removeMatching = function(deleteValue) {
     }
   }
   return this;
+};*/
+function removeFromArrayMatching(fromArray, deleteValue) {
+	for (var i = 0; i < fromArray.length; i++) {
+		if (fromArray[i] == deleteValue) {
+			fromArray.splice(i, 1);
+			i--;
+		}
+	}
 };
