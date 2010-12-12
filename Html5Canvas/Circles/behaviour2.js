57238e240a3ca7c7ca98853fe7bd8cee7ba8a449
Mark Embling <contact@markembling.info>
2010-12-12 17:36:45 +0000

    Added music. Added more styles and controls Music excluded from repo because it's massive!

--- Html5Canvas/Circles/behaviour2.js
+++ Html5Canvas/Circles/behaviour2.js
@@ -1,25 +1,78 @@
+var music = [
+	{ credit: 'Freedom Is Me by <a href="http://dig.ccmixter.org/dig?user=scottaltham">scottaltham</a>', 
+	  ogg: 'freedom-is-me.ogg', 
+	  mp3: 'freedom-is-me.mp3' },
+	{ credit: 'Without LOVE by <a href="http://dig.ccmixter.org/dig?user=Loveshadow">Loveshadow</a>', 
+	  ogg: 'without-love.ogg', 
+	  mp3: 'without-love.mp3' },
+	{ credit: 'The Sweetest Sin by <a href="http://dig.ccmixter.org/dig?user=Loveshadow">Loveshadow</a>', 
+	  ogg: 'the-sweetest-sin.ogg', 
+	  mp3: 'the-sweetest-sin.mp3' },
+	{ credit: 'Glow by <a href="http://dig.ccmixter.org/dig?user=gurdonark">Gurdonark</a>', 
+	  ogg: 'glow.ogg', 
+	  mp3: 'glow.mp3' },
+	
+	// Earth by zero-project
+	{ credit: '<a href="http://www.jamendo.com/en/track/373259">Killing Earth</a> by <a href="http://www.jamendo.com/en/artist/zero-project">zero-project</a>', 
+	  ogg: 'earth/ogg/01.ogg', mp3: 'earth/mp3/01.mp3' },
+	{ credit: '<a href="http://www.jamendo.com/en/track/373268">Still Breathing</a> by <a href="http://www.jamendo.com/en/artist/zero-project">zero-project</a>', 
+	  ogg: 'earth/ogg/02.ogg', mp3: 'earth/mp3/02.mp3' },
+	{ credit: '<a href="http://www.jamendo.com/en/track/373287">Millenium Sunset</a> by <a href="http://www.jamendo.com/en/artist/zero-project">zero-project</a>', 
+	  ogg: 'earth/ogg/03.ogg', mp3: 'earth/mp3/03.mp3' },
+	{ credit: '<a href="http://www.jamendo.com/en/track/373290">Earthbeat</a> by <a href="http://www.jamendo.com/en/artist/zero-project">zero-project</a>', 
+	  ogg: 'earth/ogg/04.ogg', mp3: 'earth/mp3/04.mp3' },
+	{ credit: '<a href="http://www.jamendo.com/en/track/373293">Distorted Reality</a> by <a href="http://www.jamendo.com/en/artist/zero-project">zero-project</a>', 
+	  ogg: 'earth/ogg/05.ogg', mp3: 'earth/mp3/05.mp3' },
+	{ credit: '<a href="http://www.jamendo.com/en/track/373300">Ephemeral Living</a> by <a href="http://www.jamendo.com/en/artist/zero-project">zero-project</a>', 
+	  ogg: 'earth/ogg/06.ogg', mp3: 'earth/mp3/06.mp3' },
+	{ credit: '<a href="http://www.jamendo.com/en/track/373302">Epilogue</a> by <a href="http://www.jamendo.com/en/artist/zero-project">zero-project</a>', 
+	  ogg: 'earth/ogg/07.ogg', mp3: 'earth/mp3/07.mp3' }
+];
+
+
 $(function() {
 	var generating = false;
+	var audioPlaying = false;
 	
 	var canvas = $("#canvas");
 	var context = canvas.get(0).getContext("2d");
 	var circles = [];
 	
 	// Button events
-	$("#start").click(function(){ 
+	$("#start a").click(function(){ 
 		startAll();
 		return false;
 	});
 	
-	$("#stop").click(function(){ 
+	$("#stop a").click(function(){ 
 		stopAll();
 		return false;
 	});
 	
-	$("#more").click(function(){
+	$("#more a").click(function(){
 		if (generating)
 			generateCircles(10);
 		
+		return false;
+	});
+	
+	$("#start-music a, #change-music a").click(function(){
+		randomMusic();
+		return false;
+	});
+	
+	$("#stop-music a").click(function(){
+		stopMusic();
+		return false;
+	});
+	
+	$("#about a").click(function(){
+		$("#description").show();
+		return false;
+	});
+	
+	$("#about-close").click(function(){
+		$("#description").hide();
 		return false;
 	});
 	
@@ -27,9 +80,43 @@ $(function() {
 	function resizeCanvas() {
 		canvas.attr("width", $(window).width());
 		canvas.attr("height", $(window).height());
+	}
+	
+	// Music
+	function randomMusic() {
+		var choice = music[randomFromTo(0, music.length - 1)];
+		var audio = $('#music');
+
+		$('#music-credit').html(choice.credit);
+
+		// Determine format
+		var canPlayType = audio.get(0).canPlayType("audio/ogg");
+		var musicFile = 'music/';
+		if (canPlayType.match(/maybe|probably/i)) {
+			musicFile += choice.ogg;
+		} else {
+			musicFile += choice.mp3;
+		}
+
+		audio.attr('src', musicFile);
+		audio.get(0).play();
 		
-		//canvasWidth = canvas.width();
-		//canvasHeight = canvas.height();
+		audioPlaying = true;
+		
+		$("#start-music").hide();
+		$("#stop-music").show();
+		$("#change-music").show();
+	}
+
+	function stopMusic() {
+		$('#music').get(0).pause();
+		$('#music-credit').text('none playing');
+		
+		audioPlaying = false;
+		
+		$("#start-music").show();
+		$("#stop-music").hide();
+		$("#change-music").hide();
 	}
 	
 	function randomColour() {
@@ -76,18 +163,26 @@ $(function() {
 		generating = true; 
 		generateCircles(30);
 		
+		randomMusic();
+		
 		$("#start").hide();
 		$("#stop").show();
 		$("#more").show();
+		
+		$("#music-menu, #music-separator").show();
 	}
 	
 	function stopAll() {
 		generating = false;
 		fadeAllCircles();
 		
+		stopMusic();
+		
 		$("#stop").hide();
 		$("#more").hide();
 		$("#start").show();
+		
+		$("#music-menu, #music-separator").hide();
 	}
 	
 	function animateCircles() {
@@ -125,6 +220,11 @@ $(function() {
 		
 		// Remove all the killed ones from the circles array
 		removeFromArrayMatching(circles, null);
+		
+		// Change music if necessary
+		if (audioPlaying && $('#music').get(0).ended) {
+			randomMusic();
+		}
 	}
 	
 	// Init
@@ -136,7 +236,7 @@ $(function() {
 	
 	$(window).resize(function() { 
 		resizeCanvas();
-	});	
+	});
 });
 
 
